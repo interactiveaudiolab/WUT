9fff8b44fdb0e0ddfc762c8ee18aa86669c0255e
Nathan Shelly <nathanshelly@u.northwestern.edu>
2018-02-06 01:21:22 -0600

    Augment scaffolding with pared down js

---
+++ app/app/static/js/test.js
@@ -0,0 +1,140 @@
+var InteractiveAudioLab = namespace('InteractiveAudioLab');
+var WebUnmixingToolbox = namespace('InteractiveAudioLab.WebUnmixingToolbox');
+var Main = namespace('InteractiveAudioLab.WebUnmixingToolbox.Main');
+
+var mixture_waveform = Object.create(WaveSurfer);
+var result_waveform = Object.create(WaveSurfer);
+var all_waveforms = [mixture_waveform, result_waveform];
+var defaultZoomStart;
+var zoomStepSize = 5;
+var mixture_spectrogram_heatmap = new SpectrogramHeatmap('spectrogram-heatmap', 20000);
+var result_spectrogram_heatmap = new SpectrogramHeatmap('result-spectrogram-heatmap', 20000);
+
+var socket;
+var time_to_graph = 0.0;
+var duet_color = pink;
+var ft2d_color = yellow;
+var spec_as_image = false;
+
+$(document).ready(function() {
+	window.AudioContext = window.AudioContext || window.webkitAudioContext;
+	online = new AudioContext();
+
+	context = online;
+
+  socket_namespace = '/wut';
+  socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port + socket_namespace);
+
+  socket.on('connect', function() {
+      console.log('Socket connected.');
+  });
+
+  socket.on('audio_upload_ok', function () {
+      $('#general-status').text('Audio uploaded to server.');
+  });
+
+  socket.on('spectrogram', function (message) {
+      var data = JSON.parse(message.spectrogram);
+      make_spectrogram(mixture_spectrogram_heatmap, data, mixture_waveform.backend.getDuration());
+  });
+
+  socket.on('spectrogram_image_ready', function (message) {
+      getSpectrogramAsImage(mixture_spectrogram_heatmap, message.path, message.max_freq);
+  });
+
+  socket.on('bad_file', function () {
+      console.log('File rejected by server');
+  });
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    var mixtureOptions = {
+        container: document.querySelector('#mixture-waveform'),
+        waveColor: 'blue',
+        progressColor: 'navy',
+        cursorColor: 'black',
+        scrollParent: false,
+        height: 80,
+        normalize: true,
+        audioRate: 1.0
+    };
+
+    // Init mixture_waveform
+    mixture_waveform.init(mixtureOptions);
+    defaulZoomStart = mixture_waveform.params.minPxPerSec;
+
+    // Init result_waveform
+    var resultOptions = {
+        container: document.querySelector('#result-waveform'),
+        waveColor: 'blue',
+        progressColor: 'navy',
+        cursorColor: 'black',
+        scrollParent: false,
+        height: 120,
+        normalize: true,
+        audioRate: 1.0
+    };
+
+    result_waveform.init(resultOptions);
+});
+
+mixture_waveform.on('ready', function () {
+  var timeline = Object.create(WaveSurfer.Timeline);
+
+  timeline.init({
+    wavesurfer: mixture_waveform,
+    container: '#waveform-timeline'
+  });
+});
+
+$('#privacy-policy-link').click(function(){
+    $('#privacy-modal').modal({
+        backdrop: 'static'
+    });
+});
+
+$('#mixture-zoom-in').click(function(){
+    mixture_waveform.zoom(mixture_waveform.params.minPxPerSec + zoomStepSize);
+});
+
+$('#mixture-zoom-out').click(function(){
+    mixture_waveform.zoom(mixture_waveform.params.minPxPerSec - zoomStepSize);
+});
+
+function enableTools(enabled, className) {
+    if (enabled === true) {
+        $(className).removeClass('disabled');
+    }
+    else if (enabled === false) {
+        $(className).addClass('disabled');
+    }
+}
+
+$('#results-pill').click(function() {
+    $(this).removeClass('result-ready');
+});
+
+$('#import-audio').click(function(){
+    $('#open-modal').modal({
+        backdrop: 'static',
+        keyboard: false
+    });
+    openFileDialog();
+});
+
+$('#open-button-modal').click(function () {
+    openFileDialog();
+});
+
+function openFileDialog() {
+    $('#survey')[0].reset();
+    $('#extraction-goal').multiselect('deselectAll', false);
+    audio.import_audio();
+    $('#general-status').text('Uploading audio to server...');
+}
+
+$('#survey-done').click(function () {
+    sendSurveyResults();
+});
