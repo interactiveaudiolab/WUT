eac575367fb6ac704920d62d7f1c3080c24a9ce3
Nathan Shelly <nathanshelly@u.northwestern.edu>
2018-02-06 17:02:46 -0600

    Initial toy PCA set up

---
+++ app/app/static/js/plot_pca.js
@@ -0,0 +1,60 @@
+function make_pca(heatmap, rawData) {
+  let status = $('#general-status');
+  status.text('Drawing PCA');
+  
+  heatmap.drawHeatmap(rawData)
+  enableTools(true, '.spec-tool')
+  
+  status.text('Ready...');
+}
+
+
+
+class PCAHeatmap extends PlotlyHeatmap {
+  constructor(divID, yMax) {
+    super(divID, yMax);
+
+    // currently hardcoding in x and y range
+    // might be better dynamic
+    this.xBins = 100
+    this.yBins = 100
+
+    this.plotLayout = {
+      xaxis: {
+        title: 'Principal Component 1',
+        type: 'linear',
+        range: [0, this.xBins]
+      },
+      yaxis: {
+        title: 'Principal Component 2',
+        type: 'linear',
+        range: [0, this.yBins]
+      },
+
+      // selectability
+      dragmode: 'select',
+      selectable: 'true',
+      
+      // cosmetics
+      paper_bgcolor: '#E3F0FB',
+      plot_bgcolor: '#E3F0FB',
+      margin: this.plotMargins,
+      autosize: true,
+    }
+
+    this.emptyHeatmap()
+  }
+
+  drawHeatmap(rawData) {
+    this.rawData = rawData;
+
+    this.xTicks = arange(0, this.xBins);
+    this.yTicks = arange(0, this.yBins);
+    let data = [{ x: this.xTicks, y: this.yTicks,
+      z: this.rawData, type: 'heatmap', showscale: false}];
+    let layout = this.plotLayout;
+
+    this.plot = Plotly.newPlot(this.divID, data, layout, this.plotOptions);
+    Plotly.relayout(this.divID, { width: $(window).width() });
+  }
+}
--- app/app/static/js/plotly_plots.js
+++ app/app/static/js/plotly_plots.js
@@ -21,6 +21,9 @@ class PlotlyHeatmap {
                 let range = arguments[1].range;
 
                 let curSelection = new BoxSelection(_this.xTicks, _this.yTicks, range);
+                console.log(range)
+                console.log(`First Corner: (${range.x[0]}, ${range.y[0]})`)
+                console.log(`Opposite Corner: (${range.x[1]}, ${range.y[1]})`)
                 _this.selections.push(curSelection);
 
                 updateSelectionStatus(_this.selections.length);
