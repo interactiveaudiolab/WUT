bf813857d3d559825a2153e35636e08a26844e3b
Nathan Shelly <nathanshelly@u.northwestern.edu>
2018-02-26 13:58:16 -0600

    Fix indentation issue, improve interaction

--- app/app/static/js/plot_spectrogram_scatter.js
+++ app/app/static/js/plot_spectrogram_scatter.js
@@ -10,47 +10,63 @@ class ScatterSpectrogram extends PlotlyHeatmap {
         this.audioLength = null;
         this.freqMax = null;
 
+        this.plotLayout.hovermode = false;
+
         this.plotLayout.xaxis = {
             title: "Time (s)",
             type: "linear",
             range: [0.0, 1.0],
+            showgrid: false
         };
 
         this.plotLayout.yaxis = {
             title: "Frequency (Mel)",
             type: "linear",
             autorange: true,
-            range: [0.0, 150]
+            range: [0.0, 150],
+            showgrid: false
         };
+
+        this.plotLayout
+
+        this.DOMObject.on('plotly_selected', (eventData, data) => {
+            if(!data || !data.range) { this.clearMarkers(); }
+        });
 
         this.emptyHeatmap();
     }
 
+    clearMarkers() {
+        let data = [{x:[], y:[]}];
+        this.plot = Plotly.newPlot(this.divID, data, this.plotLayout, this.plotOptions);
+    }
+
+    addMarkers(x_marks, y_marks) {
+        // let data = [{x:[], y:[], type:'scattergl',
+        //         mode:'markers', marker: { symbol: "square", size:5, color: '#ffffff', opacity: 1 }}];
+        Plotly.addTraces(spectrogram.divID, { x: x_marks, y: y_marks, type: 'scattergl',
+            mode:'markers', marker: { size: 2, color: '#ffffff', opacity: 1 }});
+    }
+
     drawImage(url, duration, freqMax) {
-        let layout = this.plotLayout;
-        layout.xaxis.range = [0.0, 1293];
-        layout.yaxis.range = [0.0, 150];
-        layout.yaxis.autorange = false;
-        layout.hovermode = false;
-        layout.showgrid = false;
-        layout.xaxis.showgrid = false;
-        layout.yaxis.showgrid = false;
-        layout.images = [{
+        this.plotLayout.xaxis.range = [0.0, duration];
+        this.plotLayout.yaxis.range = [0.0, freqMax];
+        this.plotLayout.yaxis.autorange = false;
+        this.plotLayout.images = [{
             "source": url,
             "xref": "x",
             "yref": "y",
             "x": 0,
             "y": 0,
-            "sizex": 1293,
-            "sizey": 150,
+            "sizex": duration,
+            "sizey": freqMax,
             "xanchor": "left",
             "yanchor": "bottom",
             "sizing": "stretch",
             "layer": "below"
         }];
 
-        let data = [{x:[], y:[], type:'scattergl',
-                mode:'markers', marker: { symbol: "square", size:5, color: '#ffffff', opacity: 1 }}];
-        this.plot = Plotly.newPlot(this.divID, data, layout, this.plotOptions);
+        let data = [{x:[], y:[]}];
+        this.plot = Plotly.newPlot(this.divID, data, this.plotLayout, this.plotOptions);
     }
 }
