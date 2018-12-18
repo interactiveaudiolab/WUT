ad5c1b1a483a6456ac4d56e27c0735c95898923e
Nathan Shelly <nsnshelly@gmail.com>
2018-12-17 21:19:43 -0500

    Touch up `config.py` pass II

--- app/app/app_obj.py
+++ app/app/app_obj.py
@@ -4,7 +4,7 @@ from flask_redis import FlaskRedis
 from werkzeug.contrib.fixers import ProxyFix
 from flask_socketio import SocketIO
 
-from .config import UPLOAD_BASE_FOLDER, Config, REDIS_URL
+from .config import UPLOAD_BASE_FOLDER, SECRET_KEY, REDIS_URL
 
 # Flask app setup
 app_ = Flask(__name__)
@@ -24,4 +24,4 @@ socketio = SocketIO(app_, ping_timeout=300, ping_interval=60)
 socketio.manage_session = False
 
 app_.config['UPLOAD_FOLDER'] = UPLOAD_BASE_FOLDER
-app_.secret_key = Config.SECRET_KEY
+app_.secret_key = SECRET_KEY
--- app/app/config.py
+++ app/app/config.py
@@ -1,35 +1,7 @@
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-
-class Config(object):
-    DEBUG = False
-    TESTING = False
-    CSRF_ENABLED = True
-    SECRET_KEY = 'abcdefg1234567'
-
-
-class ProductionConfig(Config):
-    DEBUG = False
-
-
-class StagingConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
-
-
-class DevelopmentConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
-
-
-class TestingConfig(Config):
-    TESTING = True
-
-
-UPLOAD_BASE_FOLDER = os.path.join('tmp')
-UPLOAD_BASE_FOLDER = os.path.abspath(UPLOAD_BASE_FOLDER)
+SECRET_KEY = 'abcdefg1234567'
+UPLOAD_BASE_FOLDER = os.path.abspath(os.path.join('tmp'))
 USER_AUDIO_FOLDER = os.path.join(UPLOAD_BASE_FOLDER, 'user_audio')
 USER_AUDIO_ORIGINAL_FOLDER_NAME = 'original_files'
 USER_AUDIO_CONVERTED_FOLDER_NAME = 'converted_files'
